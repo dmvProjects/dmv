b2c68b05290dc54159251597b48c9274d3fb85ed
dmv <dmv.perm@gmail.com>
2025-11-07 14:46:09 +0500

    vault backup: 2025-11-07 14:46:09

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -41,6 +41,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
+    Component.RecentNotes(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
