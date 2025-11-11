45d3973a6b9ce0411e80b2e01fb3c8c9c9b80a73
dmv <dmv.perm@gmail.com>
2025-11-11 16:23:16 +0500

    vault backup: 2025-11-11 16:23:16

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -49,6 +49,23 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
+  afterBody: [
+  Component.Comments({
+    provider: 'giscus',
+    options: {
+      // from data-repo
+      repo: 'dmvProjects/dmv',
+      // from data-repo-id
+      repoId: 'R_kgDOQQZEYQ',
+      // from data-category
+      category: 'Announcements',
+      // from data-category-id
+      categoryId: 'DIC_kwDOQQZEYc4CxrJU',
+      // from data-lang
+      lang: 'ru'
+      }
+    }),
+  ],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
