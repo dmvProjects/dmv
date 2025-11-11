4c75a9183b37105691a7b751194d724c449148c6
dmv <dmv.perm@gmail.com>
2025-11-11 12:58:51 +0500

    vault backup: 2025-11-11 12:58:51

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -35,7 +35,9 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      useSavedState: false,
+    }),
   ],
   right: [
     Component.RecentNotes({
