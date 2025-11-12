8bbbf20635aafd4c1e246dde3c82975fbe083ab5
dmv <dmv.perm@gmail.com>
2025-11-12 15:11:56 +0500

    vault backup: 2025-11-12 15:11:56

--- quartz.config.ts
+++ quartz.config.ts
@@ -29,7 +29,8 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
+          //light: "#faf8f8",
+          light: "#e6e4dc",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
