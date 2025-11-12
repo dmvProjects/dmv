65f0d99b0dd861fa7d39eaf63b9d6c7850a45e5f
dmv <dmv.perm@gmail.com>
2025-11-12 15:17:39 +0500

    vault backup: 2025-11-12 15:17:39

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -49,7 +49,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
-  afterBody: [
+/*  afterBody: [
   Component.Comments({
     provider: 'giscus',
     options: {
@@ -65,7 +65,7 @@ export const defaultContentPageLayout: PageLayout = {
       lang: 'ru'
       }
     }),
-  ],
+  ], */
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
