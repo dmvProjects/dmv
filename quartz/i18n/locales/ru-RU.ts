7dbf94c85e2ecf024bbf461598c1b2e5f6049394
dmv <dmv.perm@gmail.com>
2025-11-13 15:47:12 +0500

    vault backup: 2025-11-13 15:47:12

--- quartz/i18n/locales/ru-RU.ts
+++ quartz/i18n/locales/ru-RU.ts
@@ -73,9 +73,9 @@ export default {
       home: "Вернуться на главную страницу",
     },
     folderContent: {
-      folder: "Папка",
+      folder: "Раздел",
       itemsUnderFolder: ({ count }) =>
-        `в этой папке ${count} элемент${getForm(count, "", "а", "ов")}`,
+        `${count} замет${getForm(count, "", "и", "ок")}`,
     },
     tagContent: {
       tag: "Тег",
