8500d81d5886ad8e9af05780ce5dc45da91b4642
dmv <dmv.perm@gmail.com>
2025-11-19 12:02:57 +0500

    vault backup: 2025-11-19 12:02:57

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -67,7 +67,7 @@ export const defaultContentPageLayout: PageLayout = {
     }),
   ], */
   afterBody: [
-      Remark42Comments(),  // ← вставляем сюда
+      Component.Remark42Comments(),  // ← вставляем сюда
     ],
 }
 
