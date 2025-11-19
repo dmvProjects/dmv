c68d950e675532071c50240babec6784b9d08729
dmv <dmv.perm@gmail.com>
2025-11-19 15:14:07 +0500

    vault backup: 2025-11-19 15:14:07

--- quartz.config.ts
+++ quartz.config.ts
@@ -75,7 +75,14 @@ const config: QuartzConfig = {
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
-      Plugin.Remark42({ host: "https://dmv-perm.ru:65430", site_id: "жил-был-пёс", no_footer: true, components: ["embed"], locale: "ru", simple_view: true, }),
+      Plugin.Remark42({ 
+        host: "https://dmv-perm.ru:65430", 
+        site_id: "жил-был-пёс", 
+        no_footer: true, components: ["embed"], 
+        locale: "ru", 
+        simple_view: true, 
+        '--rc-background-color': '#e6e4dc',
+      }),
     ],
     filters: [Plugin.RemoveDrafts()],
     emitters: [
