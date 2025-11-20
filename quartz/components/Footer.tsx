bbce8c1e55ace89c1e728cfa334eccfa9d497d0f
dmv <dmv.perm@gmail.com>
2025-11-20 13:49:37 +0500

    vault backup: 2025-11-20 13:49:37

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -12,6 +12,19 @@ export default ((opts?: Options) => {
     const year = new Date().getFullYear()
     const links = opts?.links ?? []
     return (
+      <script
+    dangerouslySetInnerHTML={{
+        __html: `
+        var remark_config = {
+            theme: window.matchMedia && window.matchMedia('(prefers-color-scheme:dark)').matches ? "dark" : "light",
+        };
+        window.matchMedia('(prefers-color-scheme:dark)').addEventListener('change', event => {
+            const newColorScheme = event.matches ? "dark" : "light";
+            window.REMARK42.changeTheme(newColorScheme);
+        });
+        `,
+    }}
+></script>
       <footer class={`${displayClass ?? ""}`}>
         <p>
           {"Жил-был Пёс"}{" "}
