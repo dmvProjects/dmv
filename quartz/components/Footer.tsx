389638195116c5e156f1920ec0e161ffd9e8afce
dmv <dmv.perm@gmail.com>
2025-11-20 13:56:53 +0500

    vault backup: 2025-11-20 13:56:53

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -12,35 +12,19 @@ export default ((opts?: Options) => {
     const year = new Date().getFullYear()
     const links = opts?.links ?? []
     return (
-      <>
-        <footer class={`${displayClass ?? ""}`}>
-          <p>
-            {"Жил-был Пёс"} © {year}
-          </p>
-          <ul>
-            {Object.entries(links).map(([text, link]) => (
-              <li>
-                <a href={link}>{text}</a>
-              </li>
-            ))}
-          </ul>
-        </footer>
-
-        {/* Скрипт для Remark42 */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              var remark_config = {
-                  theme: window.matchMedia && window.matchMedia('(prefers-color-scheme:dark)').matches ? "dark" : "light",
-              };
-              window.matchMedia('(prefers-color-scheme:dark)').addEventListener('change', event => {
-                  const newColorScheme = event.matches ? "dark" : "light";
-                  window.REMARK42.changeTheme(newColorScheme);
-              });
-            `,
-          }}
-        ></script>
-      </>
+      <footer class={`${displayClass ?? ""}`}>
+        <p>
+          {"Жил-был Пёс"}{" "}
+          © {year}
+        </p>
+        <ul>
+          {Object.entries(links).map(([text, link]) => (
+            <li>
+              <a href={link}>{text}</a>
+            </li>
+          ))}
+        </ul>
+      </footer>
     )
   }
 
