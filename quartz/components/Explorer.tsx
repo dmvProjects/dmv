471f28616332c84c60e872f9b78c902df8c4a22f
dmv <dmv.perm@gmail.com>
2025-11-21 15:15:58 +0500

    vault backup: 2025-11-21 15:15:58

--- quartz/components/Explorer.tsx
+++ quartz/components/Explorer.tsx
@@ -8,6 +8,7 @@ import { i18n } from "../i18n"
 import { FileTrieNode } from "../util/fileTrie"
 import OverflowListFactory from "./OverflowList"
 import { concatenateResources } from "../util/resources"
+import RecentNotes from "./RecentNotes"  // путь к вашему компоненту RecentNotes
 
 type OrderEntries = "sort" | "filter" | "map"
 
@@ -139,6 +140,9 @@ export default ((userOpts?: Partial<Options>) => {
         </button>
         <div id={id} class="explorer-content" aria-expanded={false} role="group">
           <OverflowList class="explorer-ul" />
+        </div>
+        <div class="recent-notes-wrapper">
+          <RecentNotes />
         </div>
         <template id="template-file">
           <li>
