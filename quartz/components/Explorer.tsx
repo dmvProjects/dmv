f14827b61495d57821dc60f0223816dd87359964
dmv <dmv.perm@gmail.com>
2025-11-21 14:53:05 +0500

    vault backup: 2025-11-21 14:53:05

--- quartz/components/Explorer.tsx
+++ quartz/components/Explorer.tsx
@@ -144,7 +144,6 @@ export default ((userOpts?: Partial<Options>) => {
         <div class="recent-notes-wrapper">
           <RecentNotes />
         </div>
-      </div>
         <template id="template-file">
           <li>
             <a href="#"></a>
