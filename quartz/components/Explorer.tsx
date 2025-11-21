7cda6f647a0794564195c3587e1c333dee344525
dmv <dmv.perm@gmail.com>
2025-11-21 14:44:51 +0500

    vault backup: 2025-11-21 14:44:51

--- quartz/components/Explorer.tsx
+++ quartz/components/Explorer.tsx
@@ -8,6 +8,7 @@ import { i18n } from "../i18n"
 import { FileTrieNode } from "../util/fileTrie"
 import OverflowListFactory from "./OverflowList"
 import { concatenateResources } from "../util/resources"
+import RecentNotes from "./RecentNotes"  // путь к вашему компоненту RecentNotes
 
 type OrderEntries = "sort" | "filter" | "map"
 
